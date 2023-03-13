df4bb99f9979d1c70429d9326ab2193f20c082f3
TorrentialFire <wilsontfwi@gmail.com>
2023-03-13 00:35:42 -0500

    Adds base.html template for pages within the app (more)
    Adds detail view for Questions and updates Questions index to list latest questions

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import get_object_or_404, render
+
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
+    #return HttpResponse("Hello, world. You're at the polls index.")
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question {}.".format(question_id))
+    #try:
+    #    question = Question.objects.get(pk=question_id)
+    #except Question.DoesNotExist:
+    #    raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+    #return HttpResponse("You're looking at question {}.".format(question_id))
 
 def results(request, question_id):
     return HttpResponse("You're looking at the results of question {}.".format(question_id))
